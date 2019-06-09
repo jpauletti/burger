7fe573cb261be5ec941decd99942cbdbef96d240
Julia Pauletti <julia.pauletti@yahoo.com>
2019-06-09 18:53:41 -0500

    header section of page, server working, add css and image files

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,3 +5,13 @@ var router = express.Router();
 
 // export routes
 module.exports = router;
+
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var allBurgers = {
+            burgers: data
+        };
+        console.log(allBurgers);
+        res.render("index", allBurgers);
+    })
+})
