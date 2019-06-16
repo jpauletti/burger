ae70e5ace80ff41d70c99513ee57f06ef9eb6b2e
Julia Pauletti <julia.pauletti@yahoo.com>
2019-06-15 19:43:56 -0500

    code cleanup and readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# burger
+# Eat&bull;a&bull;Burger
+
+This is a restaurant app - users can input the names of burgers they'd like to eat.
+
+There is a queue of burgers to be eaten, and a list of burgers that have already been devoured.
+
+Once a burger is added to the queue, you can click the "Devour" button, and it will be moved to the Devoured list.
+
+This app was built with Node.js, Express, MySQL, and Handlebars.
+
+[See it deployed ->](https://sleepy-temple-70496.herokuapp.com/)
+
+![screenshot](public/assets/images/eat-a-burger.png)
--- config/orm.js
+++ config/orm.js
@@ -1,36 +1,5 @@
 var connection = require("./connection.js");
 
-// creates array of question marks to prevent sql injection
-// function printQuestionMarks(num) {
-//     var arr = [];
-
-//     for (var i = 0; i < num; i++) {
-//         arr.push("?");
-//     }
-
-//     return arr.toString();
-// }
-
-// function to convert object key/value pairs to SQL syntax
-// function objToSql(ob) {
-//     var arr = [];
-
-//     // loop through the keys and push the key/value as a string int arr
-//     for (var key in ob) {
-//         var value = ob[key];
-//         // check to skip hidden properties
-//         if (Object.hasOwnProperty.call(ob, key)) {
-//             // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-//             if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//                 value = "'" + value + "'";
-//             }
-//             // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-//             // e.g. {sleepy: true} => ["sleepy=true"]
-//             arr.push(key + "=" + value);
-//         }
-// }
-
-
 var orm = {
     selectAll: function(tableName, callback) {
         var query = "SELECT * FROM " + tableName;
@@ -42,19 +11,6 @@ var orm = {
             callback(result);
         })
     },
-
-    // insertOne: function(tableName, colName, val, callback) {
-    //     query = "INSERT INTO " + tableName;
-    //     query += " (" + colName + ") ";
-    //     query += "VALUES (";
-    //     query += printQuestionMarks(val.length);
-    //     query += ") ";
-
-    //     connection.query(query, vals, function (err, result) {
-    //         if (err) throw err;
-    //         callback(result);
-    //     })
-    // },
 
     insertOne: function (tableName, colName, val, callback) {
         query = "INSERT INTO " + tableName;
@@ -70,19 +26,6 @@ var orm = {
             callback(result);
         })
     },
-
-    // An example of objColVals would be {name: panther, sleepy: true}
-    // updateOne: function (tableName, objColVals, condition, callback) {
-    //     query = "UPDATE " + tableName;
-    //     query += " SET " + objToSql(objColVals);
-    //     query += " WHERE " + condition;
-
-    //     connection.query(query, function (err, result) {
-    //         if (err) throw err;
-    //         callback(result);
-    //     })
-
-    // },
 
     updateOne: function (tableName, colName, colValue, condition, callback) {
         query = "UPDATE " + tableName;
